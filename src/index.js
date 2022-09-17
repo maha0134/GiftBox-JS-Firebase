c744f549afc64b1f513d1032229344a8c6c55012
AKSHAY MAHAJAN <akshaymahajan@AKSHAYs-MacBook-Pro.local>
2022-09-17 17:13:16 -0400

    ideas can be added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
   getDocs,
   query,
   where,
+  addDoc,
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -22,40 +23,57 @@ const app = initializeApp(firebaseConfig);
 // get a reference to the database
 const db = getFirestore(app);
 
+//global variables
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const people = []; //to hold all the people from the collection
+
 document.addEventListener("DOMContentLoaded", () => {
   //set up the dom events
-  // document
-  //   .getElementById("btnCancelPerson")
-  //   .addEventListener("click", hideOverlay);
-  // document
-  //   .getElementById("btnCancelIdea")
-  //   .addEventListener("click", hideOverlay);
+  document
+    .getElementById("btnCancelPerson")
+    .addEventListener("click", hideOverlay);
+  document
+    .getElementById("btnCancelIdea")
+    .addEventListener("click", hideOverlay);
   // document.querySelector(".overlay").addEventListener("click", hideOverlay);
 
-  // document
-  //   .getElementById("btnAddPerson")
-  //   .addEventListener("click", showOverlay);
-  // document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
+  document
+    .getElementById("btnAddPerson")
+    .addEventListener("click", showOverlay);
+  document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
+  document.getElementById("btnSaveIdea").addEventListener("click", saveIdea);
+  document
+    .getElementById("btnSavePerson")
+    .addEventListener("click", savePerson);
 
   getPeople();
 });
 
-// function hideOverlay(ev) {
-//   ev.preventDefault();
-//   document.querySelector(".overlay").classList.remove("active");
-//   document
-//     .querySelectorAll(".overlay dialog")
-//     .forEach((dialog) => dialog.classList.remove("active"));
-// }
-// function showOverlay(ev) {
-//   ev.preventDefault();
-//   document.querySelector(".overlay").classList.add("active");
-//   const id = ev.target.id === "btnAddPerson" ? "dlgPerson" : "dlgIdea";
-//   //TODO: check that person is selected before adding an idea
-//   document.getElementById(id).classList.add("active");
-// }
-
-const people = []; //to hold all the people from the collection
+function hideOverlay() {
+  document.querySelector(".overlay").classList.remove("active");
+  document
+    .querySelectorAll(".overlay dialog")
+    .forEach((dialog) => dialog.classList.remove("active"));
+}
+function showOverlay(ev) {
+  document.querySelector(".overlay").classList.add("active");
+  const id = ev.target.id === "btnAddPerson" ? "dlgPerson" : "dlgIdea";
+  //TODO: check that person is selected before adding an idea
+  document.getElementById(id).classList.add("active");
+}
 
 async function getPeople() {
   //call this from DOMContentLoaded init function
@@ -81,20 +99,6 @@ function buildPeople(people) {
   //build the HTML
   let ul = document.querySelector("ul.person-list");
   ul.addEventListener("click", personClicked);
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
   let index = 0;
   //replace the old ul contents with the new.
   ul.innerHTML = "";
@@ -135,7 +139,7 @@ async function getIdeas(id) {
   ul.innerHTML = "";
   if (gifts.length > 0) {
     ul.innerHTML = gifts.map((gift) => {
-      return `<li class="idea" data-id = ${gift.id}><label for="chk-uniqueid"><input type="checkbox" id="chk-uniqueid" /> Bought</label>
+      return `<li class="idea" id="" data-id = ${gift.id}><label for="chk-uniqueid"><input type="checkbox" id="chk-uniqueid" /> Bought</label>
       <p class="title">${gift.idea}</p>
       <p class="location">${gift.location}</p>`;
     });
@@ -151,5 +155,97 @@ function personClicked(ev) {
     clickedPerson.classList.add("active");
     const id = clickedPerson.dataset.id;
     getIdeas(id);
+  }
+}
+
+async function savePerson() {
+  //function called when user clicks save button from person dialog
+  let name = document.getElementById("name").value;
+  let month = document.getElementById("month").value;
+  let day = document.getElementById("day").value;
+  if (!name || !month || !day) return; //form needs more info
+  const person = {
+    name,
+    "birth-month": month,
+    "birth-day": day,
+  };
+  try {
+    const docRef = await addDoc(collection(db, "people"), person);
+    console.log("Document written with ID: ", docRef.id);
+    //1. clear the form fields
+    document.getElementById("name").value = "";
+    document.getElementById("month").value = "";
+    document.getElementById("day").value = "";
+    //2. hide the dialog and the overlay
+    hideOverlay();
+    //3. display a message to the user about success
+    tellUser(`Person ${name} added to database`);
+    person.id = docRef.id;
+    //4. ADD the new HTML to the <ul> using the new object
+    showPerson(person);
+  } catch (err) {
+    console.error("Error adding document: ", err);
+    //do you want to stay on the dialog?
+    //display a mesage to the user about the problem
+  }
+}
+//TODO
+function tellUser() {}
+
+function showPerson(person) {
+  let li = document.getElementById(person.id);
+  if (li) {
+    //update on screen
+    const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
+    //Use the number of the birth-month less 1 as the index for the months array
+    //replace the existing li with this new HTML
+    li.outerHTML = `<li data-id="${person.id}" class="person">
+            <p class="name">${person.name}</p>
+            <p class="dob">${dob}</p>
+          </li>`;
+  } else {
+    //add to screen
+    const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
+    //Use the number of the birth-month less 1 as the index for the months array
+    li = `<li data-id="${person.id}" class="person">
+            <p class="name">${person.name}</p>
+            <p class="dob">${dob}</p>
+          </li>`;
+    document.querySelector("ul.person-list").innerHTML += li;
+  }
+}
+
+async function saveIdea() {
+  //function called when user clicks save button from person dialog
+  let title = document.getElementById("title").value;
+  let location = document.getElementById("location").value;
+  // let day = document.getElementById("day").value;
+  if (!title || !location) return; //form needs more info
+  const selectedPerson = document.querySelector(".person.active");
+  const personId = selectedPerson.dataset.id;
+  const giftIdea = {
+    idea: title,
+    location,
+    "person-id": `/people/${personId}`,
+  };
+
+  try {
+    const docRef = await addDoc(collection(db, "gift-ideas"), giftIdea);
+    console.log("Document written with ID: ", docRef.id);
+    //1. clear the form fields
+    document.getElementById("title").value = "";
+    document.getElementById("location").value = "";
+    // document.getElementById("day").value = "";
+    //2. hide the dialog and the overlay
+    hideOverlay();
+    //3. display a message to the user about success
+    tellUser(`Gift idea ${title} added to database`);
+    // giftIdea.id = docRef.id;
+    //4. ADD the new HTML to the <ul> using the new object
+    // showGift(giftIdea);
+  } catch (err) {
+    console.error("Error adding document: ", err);
+    //do you want to stay on the dialog?
+    //display a mesage to the user about the problem
   }
 }
